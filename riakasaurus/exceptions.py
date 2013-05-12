b34d5ed1e69ac04eae1c2c5fed2e0f43b4f5ad1b
Colin Alston <colin.alston@gmail.com>
2013-05-12 10:33:44 +0200

    Add exceptions classes

--- riakasaurus/exceptions.py
+++ riakasaurus/exceptions.py
@@ -1,5 +1,14 @@
-class TimeoutError(Exception):
+class RequestTimeout(Exception):
     """ 
         Raised to notify that an operation exceeded its timeout. 
     """
+
+class ConnectTimeout(Exception):
+    """ 
+        Raised to notify that a connection timed out
+    """
+
+class RiakPBCException(Exception):
+    """ Generic PBC exception"""
+    pass
 
